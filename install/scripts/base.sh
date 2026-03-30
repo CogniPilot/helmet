6a22e9d3184fef5a41e7a88411d3a07e9ab0c808
Yuying Ba <yuying.ba@nxp.com>
2026-03-30 13:47:16 +0200

    Add gstreamer to base install for electrode_rviz
    Signed-off-by: Yuying Ba <yuying.ba@nxp.com>

--- install/scripts/base.sh
+++ install/scripts/base.sh
@@ -71,6 +71,21 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
   valgrind \
   wget \
   xterm \
-  xz-utils
+  xz-utils \
+  libgstreamer1.0-dev \
+  libgstreamer-plugins-base1.0-dev \
+  libgstreamer-plugins-bad1.0-dev \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-libav \
+  gstreamer1.0-tools \
+  gstreamer1.0-x \
+  gstreamer1.0-alsa \
+  gstreamer1.0-gl \
+  gstreamer1.0-gtk3 \
+  gstreamer1.0-qt5 \
+  gstreamer1.0-pulseaudio
 
 # vi: ts=2 sw=2 et
